e12ca9715464e6d4d9c6af5765a572389b92891f
Matias Senger <m.senger@hotmail.com>
2021-07-18 20:55:01 +0200

    Updated test

--- tests/plotter_tests/test_histogram.py
+++ tests/plotter_tests/test_histogram.py
@@ -4,34 +4,39 @@ import numpy as np
 samples = np.random.randn(999)
 
 for plotter in grafica.manager.plotters:
-	fig = grafica.manager.new(
-		title = 'Histogram',
-		subtitle = 'All the traces use the exact same data',
-		plotter_name = plotter,
-	)
-	fig.histogram(
-		samples,
-		label = 'No args',
-	)
-	bins = 5
-	fig.histogram(
-		samples,
-		bins = bins,
-		label = f'bins = {bins}',
-	)
-	bins = [-1,-.1,.1,1,2]
-	fig.histogram(
-		samples,
-		bins = bins,
-		label = f'bins = {bins}',
-		marker = '*',
-	)
-	bins = 6
-	fig.histogram(
-		samples,
-		marker = '.',
-		label = f'bins={bins}',
-		bins = bins,
-	)
-
-upi.manager.save(mkdir=True)
+	for density in {True, False}:
+		fig = grafica.manager.new(
+			title = f'Histogram density {density}',
+			subtitle = 'All the traces use the exact same data',
+			plotter_name = plotter,
+		)
+		fig.histogram(
+			samples,
+			label = 'No args',
+			density = density,
+		)
+		bins = 5
+		fig.histogram(
+			samples,
+			bins = bins,
+			label = f'bins = {bins}',
+			density = density,
+		)
+		# ~ bins = [-1,-.1,.1,1,2]
+		# ~ fig.histogram(
+			# ~ samples,
+			# ~ bins = bins,
+			# ~ label = f'bins = {bins}',
+			# ~ marker = '*',
+			# ~ density = density,
+		# ~ )
+		bins = 6
+		fig.histogram(
+			samples,
+			marker = '.',
+			label = f'bins={bins}',
+			bins = bins,
+			density = density,
+		)
+	
+grafica.manager.save(mkdir=True)
