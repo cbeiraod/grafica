2dad4687c1f844dc69bf862d6a0d7401b583dd5a
Matias Senger <m.senger@hotmail.com>
2021-08-04 09:43:58 +0200

    Implemented some wrappers to ease the user experience

--- grafica/__init__.py
+++ grafica/__init__.py
@@ -1,3 +1,11 @@
 from .FigureManager import FigureManager
 
 manager = FigureManager()
+
+def new(*args, **kwargs):
+	"""A shorthand wrapper around grafica.manager.new"""
+	return manager.new(*args, **kwargs)
+
+def save_unsaved(*args, **kwargs):
+	"""A shorthand wrapper around grafica.manager.save_unsaved"""
+	return manager.save_unsaved(*args, **kwargs)
